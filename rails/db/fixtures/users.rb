9c62e84b054f2e0f8990c6cc031d01c2066bdc6c
miyamae <yu1ro.mya@gmail.com>
2022-02-01 20:05:28 +0900

    add: 管理者シーダー

---
+++ rails/db/fixtures/users.rb
@@ -0,0 +1,8 @@
+User.seed do |s|
+  s.id = 1
+  s.name = '管理者'
+  s.email = 'admin@example.com'
+  s.password = 'password'
+  s.password_confirmation = 'password'
+  s.confirmed_at = Time.zone.now
+end
